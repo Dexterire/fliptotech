cd9a900f8be29dbea2a83e146392b9b4fd7634b2
Dexterire <ileadenle@gmail.com>
2024-07-26 18:07:29 -1200

    new

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let me = document.querySelectorAll(".question-answer");
-// let answers = document.querySelector(".answers");
+let show = document.querySelector("#show-menu");
+let hide = document.querySelector("#hide-menu");
+let menu = document.querySelector(".net");
 
 me.forEach((fqs) => {
   fqs.addEventListener("click", () => {
     fqs.classList.toggle("answer");
     document.querySelectorAll(".fq-icon").textContent = "-";
   });
+});
+
+show.addEventListener("click", () => {
+  menu.style.display = "block";
+});
+hide.addEventListener("click", () => {
+  menu.style.display = "none";
 });
